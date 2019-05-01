a547c8a51eee5f69617ac9e7dcab9013333014ba
Mike Haney <txmikester@gmail.com>
2019-05-01 18:27:35 -0500

    wired up report generation

--- src/got_records/main/import.clj
+++ src/got_records/main/import.clj
@@ -1,6 +1,7 @@
 (ns got-records.main.import
   (:gen-class)
   (:require [got-records.import :refer [import-all]]
+            [got-records.report :as r]
             [clojure.java.io :as io]))
 
 (def sample-files ["data/data1.txt" "data/data2.txt" "data/data3.txt"])
@@ -30,16 +31,26 @@
       (println "File" file "could not be found."))))
 
 (defn process [report-type files]
-  (println "Processing"))
+  (println "Processing the following files:")
+  (doseq [filename files]
+    (println "   " filename))
+  (let [{:keys [data read skipped]} (import-all files)]
+    (println "Read " read " records.")
+    (println "Skipped " skipped " records.")
+    (cond
+      (= "1" report-type) (r/report-output-1 data)
+      (= "2" report-type) (r/report-output-2 data)
+      (= "3" report-type) (r/report-output-3 data))))
 
-(defn -main [& [report-type & files]]
-  (if report-type
-    (if (contains? #{"1" "2" "3"} report-type)
-      (let [validated-files (validate-files files)
-            files (map :file validated-files)
-            all-valid? (every? true? (map :valid validated-files))]
-        (if all-valid?
-          (process report-type files)
-          (report-file-errors validated-files)))
-      (print-usage))
-    (print-usage)))
+(defn -main [& [report-type & in-files]]
+  (let [files (if in-files in-files sample-files)]
+    (if report-type
+      (if (contains? #{"1" "2" "3"} report-type)
+        (let [validated-files (validate-files files)
+              filenames (map :file validated-files)
+              all-valid? (every? true? (map :valid validated-files))]
+          (if all-valid?
+            (process report-type filenames)
+            (report-file-errors validated-files)))
+        (print-usage))
+    (print-usage))))
---
+++ src/got_records/report.clj
@@ -0,0 +1,11 @@
+(ns got-records.report)
+
+
+(defn report-output-1 [data]
+  (println "TODO: Output from report 1"))
+
+(defn report-output-2 [data]
+  (println "TODO: Output from report 2"))
+
+(defn report-output-3 [data]
+  (println "TODO: Output from report 3"))
